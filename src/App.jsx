36e7d4be29e05d744d79777beedadc02b5de2d70
VuBach08 <vubach08092003@gmail.com>
2026-06-17 17:45:45 +0700

    upload assets

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="app-shell">
+      <section className="app-intro">
+        <h1>Ecommercial Website</h1>
+        <p>Clean React storefront setup.</p>
+      </section>
+    </main>
+  )
+}
+
+export default App
